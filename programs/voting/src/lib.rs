0e472d76b41767af73b5f704260c3d78d2c1ec4a
C <caaarloos300@gmail.com>
2022-05-12 20:26:37 +0200

    Update lib.rs

--- programs/voting/src/lib.rs
+++ programs/voting/src/lib.rs
@@ -6,7 +6,7 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 mod voting {
     use super::*;
     ///init ///
-    pub fn initialize(ctx: Context<Create>, ticket: u64) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, ticket: u64) -> Result<()> {
         let voting: &mut Account<Voting> = &mut ctx.accounts.voting;        
         voting.authority = ctx.accounts.admin.key(); 
         voting.claimed = 0;               
@@ -14,11 +14,20 @@ mod voting {
         voting.countno = 0;            
         voting.ticket = voting;
         voting.oracle = oracle_pubkey;
+        voting.winner = winner;
 
         Ok(())
     }
+    
     ///get a ticket////
-    pub fn get_ticket(ctx: Context<Submit>) ->Result<()>{
+    pub fn get_ticket(ctx: Context<Claim>) ->Result<()>{
+
+
+    }
+    pub fn yes(ctx: Context) -> Result<()>{
+
+    }
+    pub fn no(ctx: Context) -> Result<()>{
 
     }
 
@@ -29,12 +38,27 @@ mod voting {
     //
 }
 ////// CONTEXTS///////////
+pub struct Initialize<'info> {
+    #[account(init, payer = admin, space = 8 + 180)]
+    pub voting: Account<'info, Voting>,
+    #[account(mut)]
+    pub admin: Signer<'info>,    
+    pub system_program: Program<'info, System>,
+
+    pub struct Claim<'info>{
+
+    }
+}
 
 //////////////////////////////////
 #[derive(Accounts)]
 
 ////////////////////
 
-/// Accounts///////////////
-
-
+/// Accounts//////////////
+//Errors/
+#[error_code]
+pub enum ErrorCode {
+    #[msg("You are not authorized to perform this action.")]
+    Unauthorized,
+}
